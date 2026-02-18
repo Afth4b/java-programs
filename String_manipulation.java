64bea00634bbda63d2e0ed5bf039ca69ed06a28a
Afth4b <aafthabismail1234@gmail.com>
2026-02-18 14:30:34 +0530

    string manipulation

---
+++ String_manipulation.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class String_manipulation{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string for manipulation : ");
+        String str = sc.nextLine();
+
+        System.out.println("Length of the string is : "+str.length());
+
+        // System.out.println("Enter the index u want to retrieve the character : ");
+        // int index = sc.nextInt();
+        // char result = str.charAt(index);
+        // System.out.println("Character at position "+index+" is : "+result);
+
+        // System.out.println("Enter the index u want to find the unicode : ");
+        // index = sc.nextInt();
+        // int res = str.codePointAt(index);
+        // System.out.println("Unicode of character at position "+index+" is : "+res);
+
+        // System.out.println("Enter the beginning index and endind index to find the substring: ");
+        // int beg = sc.nextInt();
+        // int end = sc.nextInt();
+        // String result = str.substring(beg,end);
+        // System.out.println(result);
+
+        System.out.print("Enter another string to compare it : ");
+        str2 = sc.nextLine();
+        
+    }
+}
